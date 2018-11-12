4b5216c2b2bdd1f410d537b024c0275eccb4419b
Steven Zhao <steven@zhao.io>
2018-11-12 02:25:37 -0800

    Disabled CORS in dev environment

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const mountRoutes = require('./routes')
+const cors = require('cors')
+const dev = process.env.NODE_ENV !== 'production'
 
 const port = 8080
 
 const app = express()
-app.use(bodyParser.json());
+app.use(bodyParser.json())
+if (dev) {
+    app.use(cors())
+}
 mountRoutes(app)
 
 app.listen(port, () => {console.log("FooDB Backend Started")})
