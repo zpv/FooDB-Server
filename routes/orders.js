3985dc02c9162b078c71a28f3091113a8d86e868
Steven Z <Steven@Zhao.io>
2018-11-15 00:35:35 -0800

    Completed order history

--- routes/orders.js
+++ routes/orders.js
@@ -11,12 +11,12 @@ const router = new Router()
 // export our router to be mounted by the parent application
 module.exports = router
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   // Verify user is signed in with a proper authentication token
   const token = req.headers['authorization']
   if (!token) return res.status(401).send({auth: false, message: 'No token provided'})
   try {
-    const {id} = jwt.verify(token.split(" ")[1], process.env.SESSION_SECRET)
+    const { id } = jwt.verify(token.split(" ")[1], process.env.SESSION_SECRET)
 
     const { restaurant_id, food_items } = req.body
     const address = (await db.query('SELECT address FROM "user" WHERE user_id = $1', [id])).rows[0].address
@@ -27,6 +27,36 @@ router.post("/", async (req, res) => {
     res.status(200).send({order_id})
     // const { rows } = await db.query('SELECT name, email, phone_num FROM "user" WHERE user_id = $1', [id])
     // res.send(rows[0])
+  } catch (e) {
+    console.log(e)
+    return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+  }
+})
+
+
+router.get('/me', async (req, res) => {
+  const token = req.headers['authorization']
+  if (!token) return res.status(401).send({auth: false, message: 'No token provided'})
+  try {
+    const { id } = jwt.verify(token.split(" ")[1], process.env.SESSION_SECRET) // get user id
+    const orders = (await db.query('SELECT * FROM "order" WHERE user_id = $1', [id])).rows
+
+    res.send(orders)
+  } catch (e) {
+    console.log(e)
+    return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  const review_id = req.params.id
+  const token = req.headers['authorization']
+  if (!token) return res.status(401).send({auth: false, message: 'No token provided'})
+  try {
+    const { id } = jwt.verify(token.split(" ")[1], process.env.SESSION_SECRET) // get user id
+    const order = (await db.query('SELECT * FROM "order" WHERE order_id = $1 AND user_id = $2', [review_id, id])).rows[0]
+
+    res.send(order)
   } catch (e) {
     console.log(e)
     return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
