04726d67a56f167a0d002ebe4762465462bf8ea3
Steven Z <Steven@Zhao.io>
2018-11-14 21:53:52 -0800

    Pluralize driver endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const users = require('./users')
-const setup = require('./setup.js')
-const restaurants = require('./restaurants.js')
-const driver = require('./driver.js')
+const setup = require('./setup')
+const restaurants = require('./restaurants')
+const drivers = require('./drivers')
+const orders = require('./orders')
 
 module.exports = (app) => {
   app.use('/users', users)
   app.use('/setup', setup)
   app.use('/restaurants', restaurants)
-  app.use('/driver', driver)
+  app.use('/drivers', drivers)
+  app.use('/orders', orders)
 }
